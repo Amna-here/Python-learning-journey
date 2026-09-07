2e81ef1b293fb873aca26cf27cf533c1a9f3e21f
Amna-here <250481028+Amna-here@users.noreply.github.com>
2026-09-07 16:59:45 +0500

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,14 @@
+print("Assalam o Alaikum! This is Amna")
+
+# Day 1 - Python Basics
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: ")
+
+print("\n--- Student Information ---")
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+
+print("\nAssalam o Alaikum! This is,", name + "!")
